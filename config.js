aba90ceb55ad5841ccf51ca5cc3afe54f8e1ce79
wayne-lock <waynelock444@hotmail.com>
2026-08-03 14:09:20 -0700

    Update Supabase publishable key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.POOL_CONFIG = {
   SUPABASE_URL: "https://xglmbjaqdidnvcjerhnl.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "YOUR_PUBLISHABLE_KEY"
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_rA0betd-krlMP-_ZJknK1g_vN10Eb0c"
 };
